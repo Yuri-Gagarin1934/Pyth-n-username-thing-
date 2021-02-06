48df3792f6d0bd56ab7930f8e8a511907a246e16
Yuri-Gagarin1934 <59685068+Yuri-Gagarin1934@users.noreply.github.com>
2021-02-06 11:53:05 +0300

    Add files via upload

---
+++ trying_something.py
@@ -0,0 +1,16 @@
+from sys import argv
+script,sectret_login=argv
+def username(*argv):
+    username=arg1
+def password(*argv):
+    password=arg2
+def email(*argv):
+    email=arg3
+def birthday(*argv):
+    birthday=arg4
+username=input("username>>")
+password=input("password>>")
+email=input("email>>")
+birthday=input(">")
+User_Data=username,password,email,birthday
+print(User_Data)
